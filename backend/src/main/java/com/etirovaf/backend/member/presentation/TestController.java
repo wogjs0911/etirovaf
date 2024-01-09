53e173145a84994262b8f1f93d5eb2e115234345
wogjs0911 <wogjs0911@naver.com>
2024-01-09 23:44:45 +0900

    feat:be queryDSL 초기 설정 후 테스트

--- backend/src/main/java/com/etirovaf/backend/member/presentation/TestController.java
+++ backend/src/main/java/com/etirovaf/backend/member/presentation/TestController.java
@@ -2,7 +2,11 @@ package com.etirovaf.backend.member.presentation;
 
 import com.etirovaf.backend.member.model.entity.Member;
 import com.etirovaf.backend.member.repository.MemberRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,18 +15,22 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Transactional
 @CrossOrigin("*")
 public class TestController {
     private final MemberRepository memberRepository;
 
-    @GetMapping("/test")
-    public List<Member> getMember(){
-        return memberRepository.findAll();
+    @GetMapping("/list")
+    public ResponseEntity<List<Member>> getMember(){
+        List<Member> member = memberRepository.findByUsername("member1");
+        return new ResponseEntity<>(member, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @GetMapping ("/add")
-    public Member addMember(Member member){
-        member.setUsername("jaejae1223322");
-        return memberRepository.save(member);
+    @GetMapping ("/save")
+    public ResponseEntity<?> addMember(){
+        Member member = new Member();
+        member.setUsername("member1");
+        Integer count = memberRepository.save(member);
+        return new ResponseEntity<>(count, HttpStatus.OK);
     }
 }
