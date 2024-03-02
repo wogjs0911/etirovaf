af7301df849c716b007042cb6a3ecec2583ba669
wogjs0911 <wogjs0911@naver.com>
2024-03-02 15:38:07 +0900

    feat: 에러 로그를 Slack으로 알림 받기

--- backend/src/main/java/com/etirovaf/backend/member/presentation/TestController.java
+++ backend/src/main/java/com/etirovaf/backend/member/presentation/TestController.java
@@ -5,6 +5,7 @@ import com.etirovaf.backend.member.repository.MemberRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,17 +13,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 @Transactional
 @CrossOrigin("*")
+@Slf4j
 public class TestController {
     private final MemberRepository memberRepository;
 
     @GetMapping("/list")
     public ResponseEntity<List<Member>> getMember(){
-        List<Member> member = memberRepository.findByUsername("member1");
+        List<Member> member = memberRepository.findByUsername("34534");
+
+        if(member.isEmpty())
+                log.error("log Error level test");
+
+
         return new ResponseEntity<>(member, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
