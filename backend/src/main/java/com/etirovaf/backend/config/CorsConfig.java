4019d1bbe3922ddde1ec37a120fd05db1a4fe514
wogjs0911 <wogjs0911@naver.com>
2024-03-03 23:54:48 +0900

    feat:be CorsConfig, BaseEntity, 로그 설정 변경

---
+++ backend/src/main/java/com/etirovaf/backend/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.etirovaf.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer{
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name());;
+    }
+}
