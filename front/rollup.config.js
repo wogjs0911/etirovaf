45458cea059843608dd6225f0bebfce8097597de
wogjs0911 <wogjs0911@naver.com>
2024-05-11 22:35:22 +0900

    Revert "feat:fe Navi바 크기 변경, 카테고리 동적인 css로 재수정, rollup.config.js 제거"
    This reverts commit 5ac2cfe904d62bfabf1b0a35e6fbec5ac0d86d2b.

---
+++ front/rollup.config.js
@@ -0,0 +1,13 @@
+// import { defineConfig } from 'vite';
+//
+// export default defineConfig({
+//     input: './src/main.tsx', // Replace with your actual entry point path (e.g., './index.tsx')
+//     // ... other Vite configuration options
+//     optimizeDeps: {
+//         include: [
+//             './src/main.tsx', // Replace with your actual entry point path (e.g., './index.tsx')
+//             // ... other files to be bundled
+//         ],
+//     },
+//     // base: '/public', // Adjust the path if necessary
+// });
