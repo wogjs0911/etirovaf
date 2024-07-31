d765ac989a2de87516a47c78554ed4464fd58276
wogjs0911 <wogjs0911@naver.com>
2024-07-31 22:59:41 +0900

    build: EC2 배포 환경에서 API 요청 시, axios 경로 변경

--- front/src/apis/dream.ts
+++ front/src/apis/dream.ts
@@ -2,5 +2,5 @@ import client from "@apis/axios/client.ts";
 import {DreamValueType} from "@myTypes/dream/internal.ts";
 
 export const dream = (body: DreamValueType) => {
-    return client.post('/api/dream/reg', body);
+    return client.post('/dream/reg', body);
 };
